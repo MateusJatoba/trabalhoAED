7a5ba463887d413031ade91d39a4b892b842c996
ivad-passos <126341645+ivad-passos@users.noreply.github.com>
2023-09-21 18:53:51 -0300

    Update monstro.c

--- bibliotecas/monstro/monstro.c
+++ bibliotecas/monstro/monstro.c
@@ -13,13 +13,59 @@ void mostrarMonstro(tp_monstro *m){
     }
 }
 
+void mostrarAcao(tp_monstro m, int rodada){
+    char movSelect;
+    int ValSelect;
+    char mov[3] = {'a','d','e'};
+    int val[3] = {6,8,3};
+
+    if(rodada > 3){
+        rodada = rodada-2;
+    }
+
+    movSelect = mov[rodada-1];
+    ValSelect = val[rodada-1];
+        
+    switch (movSelect){
+        case 'a':
+            printf("\nVai Atacar= %i", ValSelect);
+            break;
+        case 'd':
+            printf("\nVai Defender= %i", ValSelect);
+            break;
+        case 'e':
+            printf("\nVai Lançar Magia= %i", ValSelect);
+            break;
+
+        default:
+            break;
+    }
+}
+
+    void passarRodada(int *rodada){
+        char s;
+        printf("\n\n\n\n PARA PASSAR A RODADA APERTE X");
+        scanf("%c", &s);
+        if(s=='x'){
+            rodada=rodada+1;
+        }
+    }
+
 int main(){
+
+    int rodada = 1;
 
     tp_monstro jeff;
     jeff.monster_id = 1;
     jeff.vida = 100;
     jeff.moveset = 1;
 
-    mostrarMonstro(&jeff);
+    while(rodada<=1){
+        mostrarMonstro(&jeff);
+        mostrarAcao(jeff,rodada);
+        passarRodada(&rodada);
+    }
+
+    printf("stop");
 
 }
